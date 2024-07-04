cd7a1449ab47b609611818e6c15031fb316614be
Annalu488 <annalu.rodrigues24@gmail.com>
2024-07-04 17:21:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,70 +2,80 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-void informacoes(){
+void informacoes() {
     cout << "O que deseja fazer no momento?" << endl;
     cout << endl;
-    cout << "-> Ligar a luz (digite L e o nivel do Brilho)" << endl;
-    cout << "-> Ligar o ar condicinado (digite A e a Temperatura desejada)" << endl;
-    cout << "-> Ligar a televisão (digite T, o Volume e o Canal desejado)" << endl;
+    cout << "-> Ligar a luz (digite LUZ e o nivel do Brilho)" << endl;
+    cout << "-> Ligar o ar condicionado (digite AR e a Temperatura desejada)" << endl;
+    cout << "-> Ligar a televisao (digite TV, o Volume e o Canal desejado)" << endl;
     cout << endl;
-    cout << "*Caso deseje desligar algo, basta adicionar o termo 'D' e depois o comando escolhido!" << endl;
-    cout << "Nesse caso ao eh necessario informar nada alem do comando de desligamento" << endl;
+    cout << "*Caso deseje desligar algo, basta adicionar o termo 'D' no final do comando escolhido!" << endl;
+    cout << "Nesse caso nao eh necessario informar nada alem do comando de desligamento" << endl;
+    cout << endl;
+    cout << "Para encerrar o programa, digite SAIR" << endl;
 }
 
-int main(){
-    char comando;
+int main() {
+    string comando;
     AutomacaoSala sala;
 
     informacoes();
 
-    cin >> comando;
+    while (true) {
+        cout << "Digite seu comando: ";
+        getline(cin, comando);
 
-    switch(comando){
-        case 'L':
+        if (comando.empty()) {
+            continue;  // Ignora comandos vazios
+        }
+
+        stringstream ss(comando);
+        string acao;
+        ss >> acao;
+
+        if (acao == "SAIR") {
+            cout << "Encerrando o programa..." << endl;
+            break;
+        } else if (acao == "LUZ") {
             int brilho;
-            cin >> brilho;
-
+            ss >> brilho;
+            if (ss.fail() || brilho < 0 || brilho > 100) {
+                cout << "Nivel de brilho invalido. Por favor, tente novamente." << endl;
+                continue;
+            }
             sala.controlarIluminacao(true, brilho);
-            break;
-        
-        case 'A':
-            int temperatura;
-            cin >> temperatura;
-
+        } else if (acao == "AR") {
+            double temperatura;
+            ss >> temperatura;
+            if (ss.fail()) {
+                cout << "Temperatura invalida. Por favor, tente novamente." << endl;
+                continue;
+            }
             sala.controlarArCondicionado(true, temperatura);
-            break;
-
-        case 'T':
+        } else if (acao == "TV") {
             float volume;
             int canal;
-            cin >> volume >> canal;
-
+            ss >> volume >> canal;
+            if (ss.fail()) {
+                cout << "Volume ou canal invalido. Por favor, tente novamente." << endl;
+                continue;
+            }
             sala.controlarTelevisao(true, volume, canal);
-            break;
-
-        case 'D':
-	    char comando_a_desligar;
-	    cin >> comando_a_desligar;
-	    
-	    if(comando_a_desligar == 'L'){
-	    	sala.controlarIluminacao(false, 0);
-            	break;
-	    }
-
-            if(comando_a_desligar == 'A'){
-	    	sala.controlarArCondicionado(false, 0);
-            	break;
-	    }
-
-	    if(comando_a_desligar == 'T'){
-	    	sala.controlarTelevisao(false, 0);
-            	break;
-	    }
+        } else if (acao == "LUZD") {
+            sala.controlarIluminacao(false, 0);
+        } else if (acao == "ARD") {
+            sala.controlarArCondicionado(false, 0);
+        } else if (acao == "TVD") {
+            sala.controlarTelevisao(false, 0, 0);
+        } else {
+            cout << "Comando invalido. Por favor, tente novamente." << endl;
+        }
     }
 
     return 0;
 }
+
